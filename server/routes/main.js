ccd667c4c4b9526c54f5023aca300d2c5df58134
RazviCodingPadawan <razvan.georgescu89@yahoo.com>
2023-11-15 21:44:57 +0100

    data added to the blog and database

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
 
 // Routes
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   const locals = {
     title: "NodeJs Blog",
     description: "Simple Blog created with NodeJs, Express & MongoDb.",
   };
-
-  res.render("index", { locals });
+  try {
+    const data = await Post.find();
+    res.render("index", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/about", (req, res) => {
