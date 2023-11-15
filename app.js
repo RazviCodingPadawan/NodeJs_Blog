4c166187c1415445669e8b2e7add54a4dd7f8959
RazviCodingPadawan <razvan.georgescu89@yahoo.com>
2023-11-15 21:26:05 +0100

    connected to mongodb atlas

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
+
 const app = express();
 const PORT = 4000 || process.env.PORT;
+
+// Connect to DB
+connectDB();
 
 app.use(express.static("public"));
 
