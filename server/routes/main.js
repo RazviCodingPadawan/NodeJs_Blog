6f7b3b0105207101e5ab1cc3aab0780fda3530df
RazviCodingPadawan <razvan.georgescu89@yahoo.com>
2023-11-16 00:06:37 +0100

    pagination added

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,17 +4,47 @@ const Post = require("../models/Post");
 
 // Routes
 router.get("/", async (req, res) => {
-  const locals = {
-    title: "NodeJs Blog",
-    description: "Simple Blog created with NodeJs, Express & MongoDb.",
-  };
   try {
-    const data = await Post.find();
-    res.render("index", { locals, data });
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.count;
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
   } catch (error) {
     console.log(error);
   }
 });
+
+// router.get("/", async (req, res) => {
+//   const locals = {
+//     title: "NodeJs Blog",
+//     description: "Simple Blog created with NodeJs, Express & MongoDb.",
+//   };
+//   try {
+//     const data = await Post.find();
+//     res.render("index", { locals, data });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 router.get("/about", (req, res) => {
   res.render("about");
