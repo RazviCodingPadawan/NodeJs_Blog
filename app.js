dfd075537f024fe5b0ff7ef5c9be1c8d17b05e1f
RazviCodingPadawan <razvan.georgescu89@yahoo.com>
2023-11-16 14:49:53 +0100

    register and login logic, jwt, bcrypt addded

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const connectDB = require("./server/config/db");
 
@@ -13,6 +16,19 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+    // cookie: { maxAge: new Date(Date.now() + 3600000) },
+  })
+);
 
 app.use(express.static("public"));
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,8 +2,30 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layouts/admin";
+const jwtSecret = process.env.JWT_SECRET;
+
+/**
+ *
+ * Check Login
+ */
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
 /**
  * GET /
  * Admin - Login Page
@@ -27,12 +49,65 @@ router.get("/admin", async (req, res) => {
 router.post("/admin", async (req, res) => {
   try {
     const { username, password } = req.body;
- 
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
 
-    res.redirect('admin')
-    
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
+
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * POST /
+ * Admin - Check Login
+ */
+router.get("/dashboard", authMiddleware, async (req, res) => {
+  res.render("admin/dashboard");
+});
+
+// router.post("/admin", async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+
+//     if (req.body.username === "admin" && req.body.password === "password") {
+//       res.send("You are logged in");
+//     } else {
+//       res.send("Wrong username or password");
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+/**
+ * POST /
+ * Admin - Register
+ */
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: "User Created", user });
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(409).json({ message: "User already in use" });
+      }
+      res.status(500).json({ message: "Internal server error" });
+    }
   } catch (error) {
     console.log(error);
   }
