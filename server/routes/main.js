4e14420321753561582a7328f8ebd8d1e79da46d
RazviCodingPadawan <razvan.georgescu89@yahoo.com>
2023-11-16 12:58:03 +0100

    select every post with id

--- server/routes/main.js
+++ server/routes/main.js
@@ -46,6 +46,27 @@ router.get("/", async (req, res) => {
 //   }
 // });
 
+/**
+ * GET /
+ * Post :id
+ */
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
+
+    res.render("post", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.get("/about", (req, res) => {
   res.render("about");
 });
