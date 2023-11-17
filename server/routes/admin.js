77e03a7199cd058cb4f9b97001f71b14925678e4
RazviCodingPadawan <razvan.georgescu89@yahoo.com>
2023-11-17 20:39:40 +0100

    logout done

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -250,7 +250,17 @@ router.put("/delete-post/:id", authMiddleware, async (req, res) => {
     res.redirect("/dashboard");
   } catch (error) {
     console.log(error);
-  } 
+  }
+});
+
+/**
+ * GET /
+ * Admin - Logout
+ */
+router.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  // res.json({ message: "Logout successful." });
+  res.redirect("/");
 });
 
 module.exports = router;
